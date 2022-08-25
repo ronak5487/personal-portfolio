6e09f651fd6e899651b73a6f22f411a90e1445d6
ronak5487 <20bee055@nith.ac.in>
2022-08-25 18:34:38 +0530

    changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,30 +1,4 @@
-import React ,{Component} from 'react'
-import Lottie from 'react-lottie'
-import animationData from '../tech.json'
 
-class  Welcome extends Component{
-  render(){
-          const defaultOptions ={
-            loop: true,
-            autoplay: true,
-            animationData: animationData,
-            renderer: 'svg'
-          }
-  
-  return (
-    <div>
- <Lottie
-  options={defaultOptions}
-  // height={500}
-  // width={500}
-  className="w-44 h-44"
-  />
-  </div>
-  )
-};
-}
-
-export default Welcome
 
 
 
